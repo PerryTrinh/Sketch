5d88ea493a410bca6e9f7490d2c84cac6802993e
Perry Trinh <perry.trinh@berkeley.edu>
2018-04-24 22:28:12 -0700

    Made canvas fit screen, sketch on mouse move

--- main.js
+++ main.js
@@ -4,15 +4,29 @@ var ctx;
 function createCanvas() {
 	sketch = document.getElementById("sketch");
 	ctx = sketch.getContext("2d");
-	sketch.addEventListener("mousemove", draw, false);
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	window.addEventListener("mousemove", draw, false);
 }
 
 function draw(event) {
-	var x = event.x - sketch.offsetLeft;
-	var y = event.y - sketch.offsetTop;
+	var position = getMousePosition(sketch, event);
+	//var x = event.x - sketch.offsetLeft;
+	//var y = event.y - sketch.offsetTop;
 	
 	ctx.beginPath();
-	ctx.arc(x, y, 3, 0, Math.PI * 2, 0);
+	ctx.arc(position.x, position.y, 3, 0, Math.PI * 2, 0);
 	ctx.fill();
-
 }
+
+function getMousePosition(sketch, event) {
+	var prePosition = sketch.getBoundingClientRect();
+	var scaleX = sketch.width / prePosition.width;
+	var scaleY = sketch.height / prePosition.height;
+
+	return {
+		x: (event.clientX - prePosition.left) * scaleX,
+		y: (event.clientY - prePosition.top) * scaleY
+	};
+}
+
